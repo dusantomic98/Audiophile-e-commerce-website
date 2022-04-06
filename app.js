f3753cb38bcbe304b921ceb3c2e129031c20f68e
Dusan Tomic <dusan.tomic998@gmail.com>
2022-04-06 21:28:44 +0200

    changes

--- app.js
+++ app.js
@@ -7,15 +7,37 @@ const wrapper = document.querySelector(".wrapper");
 const first = document.querySelector(".hamburger span:nth-child(1)");
 const second = document.querySelector(".hamburger span:nth-child(2)");
 const third = document.querySelector(".hamburger span:nth-child(3)");
+const pay = document.querySelector(".pay");
+const purchased = document.querySelector(".purchase-confirm");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   products.classList.toggle("active");
   body.classList.toggle("active");
-  header.classList.toggle("active");
   main.classList.toggle("active");
   wrapper.classList.toggle("active");
   first.classList.toggle("active");
   second.classList.toggle("active");
   third.classList.toggle("active");
+});
+
+const shopCart = document.querySelector(".shopping-cart");
+const cartSymbol = document.querySelector(".cart");
+const cartInterior = document.querySelector(".cart-interior");
+
+cartSymbol.addEventListener("click", () => {
+  cartSymbol.classList.toggle("active");
+  body.classList.toggle("active");
+  main.classList.toggle("active");
+  wrapper.classList.toggle("active");
+  shopCart.classList.toggle("active");
+  cartInterior.classList.toggle("active");
+});
+
+pay.addEventListener("click", () => {
+  body.classList.toggle("active");
+  main.classList.toggle("active");
+  wrapper.classList.toggle("active");
+  purchased.classList.toggle("active");
+  removeItem();
 });
--- cart.js
+++ cart.js
@@ -1,8 +1,17 @@
-const productsInCart = [];
+let productsInCart = JSON.parse(localStorage.getItem("AddedItems"));
+if (!productsInCart) {
+  productsInCart = [];
+}
 const itemsList = document.querySelector(".shopping-cart");
-const cartSumPrice = document.querySelector(".sum-prices");
+const cartSumPrice = "";
 const items = document.querySelectorAll(".product-info");
-const itemsNumber = document.querySelector(".Item-number");
+const cartCount = document.querySelector(".head h6");
+const total = document.querySelector(".total span");
+const totalh6 = document.querySelector(".total h6");
+const remove = document.querySelector(".remove");
+const finalCarts = document.querySelector(".final-carts");
+const grandTotal = document.querySelector(".grand-total span");
+const confirmContent = document.querySelector(".confirm-content");
 
 const countTheSumPrice = function () {
   let sum = 0;
@@ -27,36 +36,80 @@ const updateShoppingCartHTML = function () {
   localStorage.setItem("AddedItems", JSON.stringify(productsInCart));
   if (productsInCart.length > 0) {
     let result = productsInCart.map((product) => {
-      const cartCount = (itemsNumber.innerHTML =
-        "Cart" + "(" + numCount() + ")");
       return `
-  				<div class="buyItem">
-                    <div class = "head">
-                    <h6 class = "cartCount">${cartCount}</h6>
-                    <div class = "remove">Remove all</div>
-                  </div>
-                  <div class="content">
-  					    <img src="${product.image}">
-  					    <div>
-  						    <h5>${product.name}</h5>
-  						    <h6>$${product.price}</h6>
-  						</div>
-                        <div class = "cart-btn">
-  							<button class="button-minus" data-id=${product.id}>-</button>
-  							<div class="countOfProduct">${product.count}</div>
-  							<button class="button-plus" data-id=${product.id}>+</button>
-  						</div>
-
-  					</div>
-  				</div>`;
+                
+                <ul class = "shopping-carts">
+                  <li class="buyItem">
+                      
+                    <div class="content">
+                      <img src="${product.image}">
+                      <div>
+                        <h5>${product.name}</h5>
+                        <h6>$${product.price}</h6>
+                    </div>
+                    <div class = "cart-btn">
+                      <button class="button-minus" data-id=${product.id}>-</button>
+                      <div class="countOfProduct">${product.count}</div>
+                      <button class="button-plus" data-id=${product.id}>+</button>
+                  </li>
+                </ul>
+                
+                
+                    `;
     });
     itemsList.innerHTML = result.join("");
     cartSumPrice.innerHTML = "$" + countTheSumPrice();
+    cartCount.innerHTML = "Cart" + "(" + numCount() + ")";
+    total.innerHTML = "$" + countTheSumPrice();
   } else {
-    itemsList.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
-    cartSumPrice.innerHTML = "";
+    totalh6.innerHTML = "";
   }
 };
+
+const updateBoughtHTML = function () {
+  // 3
+  localStorage.setItem("boughtItems", JSON.stringify(productsInCart));
+  if (productsInCart.length > 0) {
+    let final = productsInCart.map((product) => {
+      return `
+                
+                  <li class="finalItem">
+                      
+                    <div class="final-content">
+                      <img src="${product.image}">
+                      <div>
+                        <h5>${product.name}</h5>
+                        <h6>$${product.price}</h6>
+                    </div>
+              
+                    <div class="finalCount">x${product.count}</div>
+                  </li>
+                
+                
+                
+                    `;
+    });
+    finalCarts.innerHTML = final.join("");
+    cartSumPrice.innerHTML = "$" + countTheSumPrice();
+    cartCount.innerHTML = "Cart" + "(" + numCount() + ")";
+    grandTotal.innerHTML = "$" + countTheSumPrice();
+  } else {
+    totalh6.innerHTML = "";
+  }
+};
+
+function removeItem() {
+  productsInCart = [];
+  localStorage.removeItem("AddedItems");
+  localStorage.removeItem("boughtItems");
+  $(".shopping-carts").remove();
+  $(".final-carts").remove();
+  total.innerHTML = "$" + 0;
+  cartCount.innerHTML = "Cart" + "(" + 0 + ")";
+  grandTotal.innerHTML = "$" + 0;
+}
+
+remove.addEventListener("click", removeItem);
 
 function updateProductsInCart(product) {
   // 2
@@ -67,6 +120,7 @@ function updateProductsInCart(product) {
         productsInCart[i].basePrice * productsInCart[i].count;
       return;
     }
+    numCount();
   }
   productsInCart.push(product);
 }
@@ -94,6 +148,7 @@ items.forEach((item) => {
       };
       updateProductsInCart(product);
       updateShoppingCartHTML();
+      updateBoughtHTML();
     }
   });
 });
@@ -118,7 +173,9 @@ itemsList.addEventListener("click", (e) => {
       }
     }
     updateShoppingCartHTML();
+    updateBoughtHTML();
   }
 });
 
 updateShoppingCartHTML();
+updateBoughtHTML();
